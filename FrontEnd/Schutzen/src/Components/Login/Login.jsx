55e976b784f5cfb6b7f4a346ef56ed6a5a24d399
chadhas282-afk <chadhas282@gmail.com>
2026-02-23 18:59:43 +0530

    FE- update format to login card

--- FrontEnd/Schutzen/src/Components/Login/Login.jsx
+++ FrontEnd/Schutzen/src/Components/Login/Login.jsx
@@ -5,6 +5,8 @@ const LoginPage = () => {
     <div className='min-h-screen w-full flex items-center justify-center bg-[#87b9ff] p-4'>
 
       <div className='relative w-full max-w-5xl h-150 bg-linear-to-br from-[#0047ab] to-[#002147] rounded-3xl overflow-hidden shadow-2xl flex items-center justify-center '>
+      
+      <div className='absolute top-10 left-10 w-32 h-32 bg-blue-400 opacity-20 blur-3xl rounded-full'></div>
 
 
       </div>
