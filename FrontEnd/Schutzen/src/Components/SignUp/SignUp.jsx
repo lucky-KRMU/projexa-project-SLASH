7e10a9d792d3f68f0cdf5c15e45c3ec5a79e3e68
chadhas282-afk <chadhas282@gmail.com>
2026-02-24 18:46:51 +0530

    FE - refactor SignUp component to use functional component with state management

--- FrontEnd/Schutzen/src/Components/SignUp/SignUp.jsx
+++ FrontEnd/Schutzen/src/Components/SignUp/SignUp.jsx
@@ -1,11 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Mail, Lock, User, MapPin, Phone, ShieldCheck, Github, Chrome, Facebook } from 'lucide-react';
 
-function SignUp() {
-  return (
-    <>
-    This is Sign Up page
-    </>
+const SignupPage = () => {
+  const [activeTab, setActiveTab] = useState('user');
+
+  return(
+    <div className='min-h-screen bg-linear-to-br from-blue-400 via-blue-600 to-blue-800 items-center
+    justify-center p-4 font-sans'>
+      
+      
+    </div>
   )
 }
-
-export default SignUp
