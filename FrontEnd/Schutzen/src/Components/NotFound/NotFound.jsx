5c72760957297a20330a5267ddc027cd2edfe117
chadhas282-afk <chadhas282@gmail.com>
2026-02-25 18:30:27 +0530

    FE - create main grid container

--- FrontEnd/Schutzen/src/Components/NotFound/NotFound.jsx
+++ FrontEnd/Schutzen/src/Components/NotFound/NotFound.jsx
@@ -4,7 +4,11 @@ import { Shield } from "lucide-react";
 
 const NotFound = () => {
   return (
-    <div className='min-h-screen bg-[#0a0f1b] text-white flex items-center justify-center p-6 font-sans select-none'></div>
+    <div className='min-h-screen bg-[#0a0f1b] text-white flex items-center justify-center p-6 font-sans select-none'> 
+    
+    <div ></div>
+    
+    </div>
   )
 }
 
