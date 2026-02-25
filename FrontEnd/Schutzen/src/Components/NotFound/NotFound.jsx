fb45896e27cdc99438be8e8c21f5760aea505b49
chadhas282-afk <chadhas282@gmail.com>
2026-02-25 18:33:59 +0530

    FE - add outer box with rounded borders

--- FrontEnd/Schutzen/src/Components/NotFound/NotFound.jsx
+++ FrontEnd/Schutzen/src/Components/NotFound/NotFound.jsx
@@ -6,7 +6,7 @@ const NotFound = () => {
   return (
     <div className='min-h-screen bg-[#0a0f1b] text-white flex items-center justify-center p-6 font-sans select-none'> 
     
-    <div ></div>
+    <div className='relative w-full max-w-5xl aspect-square md:aspect-video border border-white/5 rounded-[40px] flex flex-col items-center justify-center overflow-hidden bg-linear-to-b from-white/2 to-transparent'></div>
     
     </div>
   )
