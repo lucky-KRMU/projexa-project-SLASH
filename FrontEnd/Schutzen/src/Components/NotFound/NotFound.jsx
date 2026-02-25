e2efe4b7a4afcb8297260b6d6797918775341d59
chadhas282-afk <chadhas282@gmail.com>
2026-02-25 18:36:39 +0530

    FE - build base 404 typography

--- FrontEnd/Schutzen/src/Components/NotFound/NotFound.jsx
+++ FrontEnd/Schutzen/src/Components/NotFound/NotFound.jsx
@@ -6,7 +6,19 @@ const NotFound = () => {
   return (
     <div className='min-h-screen bg-[#0a0f1b] text-white flex items-center justify-center p-6 font-sans select-none'> 
     
-    <div className='relative w-full max-w-5xl aspect-square md:aspect-video border border-white/5 rounded-[40px] flex flex-col items-center justify-center overflow-hidden bg-linear-to-b from-white/2 to-transparent'></div>
+    <div className='relative w-full max-w-5xl aspect-square md:aspect-video border border-white/5 rounded-[40px] flex flex-col items-center justify-center overflow-hidden bg-linear-to-b from-white/2 to-transparent'>
+    
+    <div className='absoulute inset-0 flex justify-center gap-4 opacity-10 pointer-events-none'>
+       {[...Array(3)].map((_, i) => (
+            <div key={i} className="w-32 h-full border-x border-slate-500 flex flex-col gap-4 p-4">
+              {[...Array(6)].map((_, j) => (
+                <div key={j} className="h-4 w-full bg-slate-700 rounded-sm" />
+              ))}
+            </div>
+          ))}
+    </div>
+    
+    </div>
     
     </div>
   )
