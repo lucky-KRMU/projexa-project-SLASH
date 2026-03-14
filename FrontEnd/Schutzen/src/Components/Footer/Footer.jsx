567ca2f7571afc8f74fd8622ed005e43fdda6a4d
lucky-KRMU <2501730142@krmu.edu.in>
2026-03-14 09:42:54 +0000

    FE - add comments for instruction

--- FrontEnd/Schutzen/src/Components/Footer/Footer.jsx
+++ FrontEnd/Schutzen/src/Components/Footer/Footer.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 function Footer() {
   return (
     <>
+    {/*This is the main footer tag. All the items will be added in this only.*/}
     <footer className='h-[30vh] font-[IBM_Plex_Sans] text-3xl text-white bg-sky-700 flex items-center justify-center'>
     This is Footer
     </footer>
--- FrontEnd/Schutzen/src/Components/Home/Home.jsx
+++ FrontEnd/Schutzen/src/Components/Home/Home.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 function Home() {
   return (
     <>
+    {/* This is the main div tag used as a container tag. kindly add all the necessary contents into it only and edit it as per the design */}
     <div className="font-[IBM_Plex_Sans] text-center text-3xl m-4">
     This is Home
     </div>
