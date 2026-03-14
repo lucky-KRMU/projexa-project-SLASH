f3b8e38821daa50905508041f3ebc7af274d09e9
lucky-KRMU <2501730142@krmu.edu.in>
2026-03-14 09:23:42 +0000

    FE - Update Navbar btn padding

--- FrontEnd/Schutzen/src/Components/Header/Header.jsx
+++ FrontEnd/Schutzen/src/Components/Header/Header.jsx
@@ -4,26 +4,26 @@ import { Link, NavLink } from 'react-router'
 function Header() {
   return (
     <>
-      <header className='h-[8vh] w-full bg-linear-to-br from-[#0047ab] to-[#002147] stiky top-0'>
+      <header className='h-[10vh] w-full bg-linear-to-br from-[#0047ab] to-[#002147] stiky top-0'>
         <nav className='h-full w-full flex'>
           <h1 className='h-10em w-full font-bold text-[30px] flex flex-row gap-3 items-center justify-start p-2 uppercase'>
             <div className='text-white hover:text-blue-200 pointer-none: cursor-pointer '>
               <NavLink to="/" >Schũtzen</NavLink></div>
           </h1>
           <ul className='h-10em w-full font-bold text-[25px] flex flex-row gap-3 items-center justify-end p-2 '>
-            <li className='text-sky-50 hover:text-blue-300'>
-              <NavLink to="/" className={({ isActive }) => `${isActive ? "text-blue-400" : ""}`}>Home</NavLink>
+            <li className='text-sky-50 hover:text-blue-100'>
+              <NavLink to="/" className={({ isActive }) => `${isActive ? "text-blue-400 font-extrabold" : ""}`}>Home</NavLink>
             </li>
-            <li className='text-sky-50  hover:text-blue-200'>
-              <NavLink to="/user" className={({ isActive }) => `${isActive ? "text-blue-400" : ""}`}>User</NavLink>
+            <li className='text-sky-50  hover:text-blue-100'>
+              <NavLink to="/user" className={({ isActive }) => `${isActive ? "text-blue-400 font-extrabold" : ""}`}>User</NavLink>
             </li>
-            <li className='text-sky-50  hover:text-blue-200'>
-              <NavLink to="/find" className={({ isActive }) => `${isActive ? "text-blue-400" : ""}`}>Find</NavLink>
+            <li className='text-sky-50  hover:text-blue-100'>
+              <NavLink to="/find" className={({ isActive }) => `${isActive ? "text-blue-400  font-extrabold" : ""}`}>Find</NavLink>
             </li>
-            <li className='bg-orange-500 hover:bg-orange-600 text-black margin-[10px] p-2 rounded-[10px] transition-all'>
+            <li className='bg-orange-500 hover:bg-orange-600 text-white margin-[10px] px-2 py-1 rounded-2xl transition-all'>
               <NavLink to="/login" className={({ isActive }) => `${isActive ? "text-white" : ""}`}>login</NavLink>
             </li>
-            <li className='bg-amber-400 text-amber-950 margin-[10px]  p-2 rounded-[10px]'>
+            <li className='bg-amber-400 text-amber-950 margin-[10px]  px-2 py-1 rounded-2xl'>
               <NavLink to="/signup" className={({ isActive }) => `${isActive ? "text-white" : ""}`}>signup</NavLink>
             </li>
           </ul>
