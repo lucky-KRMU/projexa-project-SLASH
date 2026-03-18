f05f342a2baafbc65a1111d8989991a2cdf0c685
chadhas282-afk <chadhas282@gmail.com>
2026-03-18 18:46:21 +0530

    BE: uncomment description field in user schema definition

--- BackEnd/models/User.js
+++ BackEnd/models/User.js
@@ -2,15 +2,8 @@ import mongoose from 'mongoose';
 
 const userSchema = new mongoose.Schema({
   userName: { type: String, required: true, unique: true },
-  // description: { type: String },
-  // firstName: { type: String, required: true },
-  // middleName: { type: String },
-  // email: { type: String },
-  // lastName: { type: String, required: true },
-  // phoneNumber: { type: String, required: true },
-  // location: { type: String, required: true },
-  // priceIdeal: { type: Number, required: true },
-  // gigWorkType: { type: String, required: true }
+  description: { type: String },
+  
 }, { timestamps: true });
 
 export const User = mongoose.model('User', userSchema);
