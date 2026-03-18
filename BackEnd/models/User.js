2ce3ab3770c4c0e64834900a0e0d18642962501a
chadhas282-afk <chadhas282@gmail.com>
2026-03-18 18:41:55 +0530

    BE: add initial route and model files, update dependencies, and create .env configuration

---
+++ BackEnd/models/User.js
@@ -0,0 +1,16 @@
+// import mongoose from 'mongoose';
+
+// const userSchema = new mongoose.Schema({
+//   userName: { type: String, required: true, unique: true },
+//   description: { type: String }, // small self description (optional)
+//   firstName: { type: String, required: true },
+//   middleName: { type: String }, // (optional)
+//   email: { type: String }, // email (optional)
+//   lastName: { type: String, required: true },
+//   phoneNumber: { type: String, required: true },
+//   location: { type: String, required: true },
+//   priceIdeal: { type: Number, required: true }, // price ideal (per hour)
+//   gigWorkType: { type: String, required: true } // e.g., security guard, bouncer
+// }, { timestamps: true });
+
+// export const User = mongoose.model('User', userSchema);
