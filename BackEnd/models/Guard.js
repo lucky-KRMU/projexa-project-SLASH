05cb952f681598c5f07f1cc5c8515e86d1de8af1
chadhas282-afk <chadhas282@gmail.com>
2026-03-19 18:40:35 +0530

    BE: add middleName field to guard schema definition

--- BackEnd/models/Guard.js
+++ BackEnd/models/Guard.js
@@ -4,6 +4,7 @@ const guardSchema = new mongoose.Schema({
   userName: { type: String, required: true, unique: true },
   selfDescription: { type: String },
   firstName: { type: String, required: true },
+  middleName: { type: String }, 
   
 }, { timestamps: true });
 
