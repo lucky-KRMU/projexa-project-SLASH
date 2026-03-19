1641469aa6037b0a28a73d0f6eaa09dca0dbe647
chadhas282-afk <chadhas282@gmail.com>
2026-03-19 18:39:55 +0530

    BE: uncomment userName field in guardSchema definition in Guard model

--- BackEnd/models/Guard.js
+++ BackEnd/models/Guard.js
@@ -1,16 +1,8 @@
 import mongoose from 'mongoose';
 
 const guardSchema = new mongoose.Schema({
-//   userName: { type: String, required: true, unique: true },
-//   selfDescription: { type: String }, // small self description
-//   firstName: { type: String, required: true },
-//   middleName: { type: String }, // (optional)
-//   lastName: { type: String, required: true },
-//   phoneNumber: { type: String, required: true },
-//   location: { type: String, required: true },
-//   priceIdeal: { type: Number, required: true }, // price ideal (per hour)
-//   gigWork: { type: String, required: true }, // bouncer, etc.
-//   rating: { type: Number, default: 0 } // rating
+  userName: { type: String, required: true, unique: true },
+  
 }, { timestamps: true });
 
 // export const Guard = mongoose.model('Guard', guardSchema);
