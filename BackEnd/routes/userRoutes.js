2ccc430dfafbf00d7f0d5000b72a5fc2574ffbac
chadhas282-afk <chadhas282@gmail.com>
2026-03-20 18:11:02 +0530

    BE: add GET route to fetch all users in userRoutes.js

--- BackEnd/routes/userRoutes.js
+++ BackEnd/routes/userRoutes.js
@@ -3,3 +3,14 @@ import { User } from '../models/User.js';
 
 
 const router = express.Router();
+
+
+router.get('/', async (req, res) => {
+  try {
+    const users = await User.find();
+    res.json(users);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
